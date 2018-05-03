1107d8028566289ea78df1e56d4922b852dfaf3f
Nic <njcannington@gmail.com>
2018-05-03 13:35:37 -0400

    update bootstrap

--- web/index.php
+++ web/index.php
@@ -2,13 +2,19 @@
 require "../autoload.php";
 
 use Lib\Bootstrap\Bootstrap;
+use Lib\Db\Db;
 
+// grabs URI
 if (!isset($_GET["q"])) {
     $uri = 'index';
 } else {
     $uri = $_GET["q"];
 }
 
+// initialize database
+$db = Db::getInstance();
+
+// routing
 $bootstrap = new Bootstrap($uri);
 $action = $bootstrap->request->action."Action";
 $data = $bootstrap->controller->$action();
